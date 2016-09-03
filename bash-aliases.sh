302a00d04dc91f1700876312a59c837bfe12f990
nickpyett <contact@nickpyett.com>
2016-09-03 19:09:20 +0100

    Add short bash aliases of gitish tools

---
+++ bash-aliases.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+alias adda="gitish::add_a"
+alias addn="gitish::add_n"
+alias bran="gitish::branch_name"
+alias coma="git add -A && git commit"
+alias comi="git commit"
+alias newb="gitish::branch_new"
+alias setu="gitish::set_upstream"
+alias stsn="gitish::status_n"
+alias unsn="gitish::unstage_n"
