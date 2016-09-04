2fac5f897775f869d3c1e20692c1c2a3b0d4887a
nickpyett <contact@nickpyett.com>
2016-09-04 18:22:25 +0100

    Add alias for commit_n, checking out branches, diff files

--- bash-aliases.sh
+++ bash-aliases.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+#
+# Short bash aliases for git.
 
 alias adda="gitish::add_a"
 alias addn="gitish::add_n"
 alias bran="gitish::branch_name"
+alias chod="git checkout develop"
+alias chom="git checkout master"
 alias coma="git add -A && git commit"
 alias comi="git commit"
+alias comn="gitish::commit_n"
+alias difh="git diff HEAD > gitish-diff-head.diff && open gitish-diff-head.diff"
 alias newb="gitish::branch_new"
 alias setu="gitish::set_upstream"
 alias stsn="gitish::status_n"
