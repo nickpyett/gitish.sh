f52594585844137969ff4f5bff32c17100383f3d
nickpyett <contact@nickpyett.com>
2024-08-13 20:25:41 +0100

    Rename aliases file and add shortcuts for branch_n and checkout_n

--- aliases.sh
+++ aliases.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #
-# Short bash aliases for git.
+# Short bash aliases for gitish.sh
 
 alias adda="gitish::add_a"
 alias addn="gitish::add_n"
 alias bran="gitish::branch_name"
+alias brnn="gitish::branch_n"
 alias chod="git checkout develop"
-alias chom="git checkout master"
+alias chom="git checkout main"
+alias chon="gitish::checkout_n"
 alias coma="git add -A && git commit"
 alias comi="git commit"
 alias comn="gitish::commit_n"
